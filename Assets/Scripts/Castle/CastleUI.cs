b52ef537dfdb9d2b6d09ec6d4d5db4b0bccad683
KhetagAb <31622023+KhetagAb@users.noreply.github.com>
2020-04-24 01:53:34 +0300

    New Display System[unbuged]

---
+++ Assets/Scripts/Castle/CastleUI.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class CastleUI : CastleArrow, IPointerClickHandler, IPointerEnterHandler, IPointerExitHandler {
+    public void OnPointerClick(PointerEventData eventData) {
+        if (!isDeny[index]) {
+            Castling.instance.Castle(index);
+        }
+    }
+
+    public void OnPointerEnter(PointerEventData eventData) {
+        if (!isDeny[index]) {
+            MeshRenderer mesh = GetComponentInChildren<MeshRenderer>();
+            mesh.material = Prefabs.instance.allowCastle;
+
+            showAlphaAndTile();
+        }
+    }
+    public void OnPointerExit(PointerEventData eventData) {
+        if (!isDeny[index]) {
+            MeshRenderer mesh = GetComponentInChildren<MeshRenderer>();
+            mesh.material = Prefabs.instance.defaultCastle;
+
+            hideAlphaAndTile();
+        }
+    }
+}
